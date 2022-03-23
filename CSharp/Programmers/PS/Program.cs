6b432eff46583bc23abd65622e3fe23365cce121
99lsh <99leesanghyun@gmail.com>
2022-03-23 16:55:28 +0900

    Level 1, 신고 결과 받기

--- CSharp/Programmers/PS/Program.cs
+++ CSharp/Programmers/PS/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace PS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 2022 KAKA0 BLIND RECRUITMENT 신고 결과 받기
+            var result = (new PS.Level1.RecvReportResult.Solution())
+                .solution(
+                new string[] { "muzi", "frodo", "apeach", "neo" }, 
+                new string[] { "muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi" }, 
+                2);
+
+            foreach (var item in result)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
